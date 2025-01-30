49c5834619a0cc43a8f71213308e08e3010ee4a2
Marvin Kaiser <marvin.kaiser@gmail.com>
2025-01-30 17:42:47 +0100

    Data Types

---
+++ data_types.py
@@ -0,0 +1,11 @@
+mynumber:int = int(30)
+mystring:str = str("My Cool String")
+myfloat:float = float(23.223)
+mybool: bool = bool(False)
+mybytes : bytes = bytes(5)
+mytuple: tuple = tuple((1, False , "Banana"))
+mydict: dict[str,int] = {
+    "first": 3,
+    "second": 5
+}
+print(mydict)
